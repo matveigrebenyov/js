2555aca76b268adb4a379969802ae2ad1d248fe4
Maga <mgrebenv66@mail.ru>
2021-08-12 11:13:52 +0300

    first practica

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
+const numberOfFims = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFims,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('один из последних просмотренных фильмов?', ''),
+      b = prompt('Нас сколько оцените его?', ''),
+      c = prompt('один из последних просмотренных фильмов?', ''),
+      d = prompt('Нас сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
