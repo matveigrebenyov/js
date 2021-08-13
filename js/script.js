518189ac74349c00e15402d52ea72d5747606847
Maga <mgrebenv66@mail.ru>
2021-08-13 10:51:34 +0300

    cycle

--- js/script.js
+++ js/script.js
@@ -33,9 +33,9 @@ for (let i =0; i < 2; i++){
 
 if (personalMovieDB.count < 10){
     console.log('Просмотрено довольно мало фильмов');
-}else if (personalMovieDB.count > 10 && personalMovieDB.count < 30 ) {
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
     console.log('Вы классический зритель');
-}else if(personalMovieDB.count > 30) {
+}else if(personalMovieDB.count >= 30) {
     console.log('Вы киноман');
 }else {
     console.log('Произошла ошибка');
