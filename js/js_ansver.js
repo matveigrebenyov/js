624d51c03d9b1f467d520ebc3415e120607b2233
Maga <mgrebenv66@mail.ru>
2021-08-13 10:50:18 +0300

    cycle practica

---
+++ js/js_ansver.js
@@ -0,0 +1,69 @@
+"use strict";
+
+/* 
+alert -  выводит на экран окно с сообщением и приостанавливает выполнение скрипта, пока пользователь не нажмёт «ОК». 
+promt - Она выводит модальное окно с заголовком title, полем для ввода текста, заполненным строкой по умолчанию 
+default и кнопками OK/CANCEL. 
+confirm - confirm выводит окно с вопросом question с двумя кнопками: OK и CANCEL. */
+
+
+/* 
+if (4 == 9){
+    console.log('jdf');
+} else{
+    console.log('error');
+}
+
+
+if (num < 49){
+    console.log('error');
+}else if(num > 100){
+    console.log("a lot");
+}else{
+    console.log('ok');
+}
+
+(num === 50) ? console.log('ok') : console.log('error'); */
+
+/* const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 50:
+        console.log('ok');
+        break;
+    default:
+        console.log('aaaa');
+        break;
+} */
+
+
+
+/* while (num < 55){
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55); */
+
+/* let num = 50;
+
+for (let i = 1; i < 8; i++) {
+    if (i === 6){
+        continue;
+    }
+
+    console.log(i);
+    
+} */
+/*  continue - похволяет пропускать шаг, котороый не нужен. break - полностью прирывает цикл. 
+switch только для прямого сравнения */
--- js/script.js
+++ js/script.js
@@ -9,13 +9,40 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('один из последних просмотренных фильмов?', ''),
+/* const a = prompt('один из последних просмотренных фильмов?', ''),
       b = prompt('Нас сколько оцените его?', ''),
       c = prompt('один из последних просмотренных фильмов?', ''),
       d = prompt('Нас сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+personalMovieDB.movies[c] = d; */
+
+for (let i =0; i < 2; i++){
+    const a = prompt('один из последних просмотренных фильмов?', ''),
+          b = prompt('Нас сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }else {
+        console.log('error');
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+}else if (personalMovieDB.count > 10 && personalMovieDB.count < 30 ) {
+    console.log('Вы классический зритель');
+}else if(personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+}else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
+
+
 
